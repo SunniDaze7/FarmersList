b7b5b2c04cb91382f9e980fa8b9e650b8028831c
Johnson <kameron.johnson@hp.com>
2023-06-20 16:40:13 -0500

    updated customer.js to add the module export

--- classes/Customer.js
+++ classes/Customer.js
@@ -10,25 +10,4 @@ class Customer {
     }
 }
 
-const melanie = new Customer("Melanie", "melanie@gmail.com", "22 Main St");
-const strawberries = new Product("Strawberries", 5, "The freshest fresas on the market");
-const carrots = new Product("Carrots", 2, "Perfect for an afternoon snack");
-const mangos = new Product("Mangos", 3, "The tastiest fruit you can buy");
-
-const myFirstOrder = new Cart();
-myFirstOrder.addProduct(mangos);
-myFirstOrder.addProduct(carrots);
-
-const mySecondOrder = new Cart();
-mySecondOrder.addProduct(strawberries);
-
-melanie.addToOrderHistory(myFirstOrder);
-melanie.addToOrderHistory(mySecondOrder);
-
-melanie.orderHistory; 
-/*
-[ 
-    Cart { products: [ [Product], [Product] ], total: 5 },
-    Cart { products: [ [Product] ], total: 5 },
-]
-*/
+module.exports = Customer
